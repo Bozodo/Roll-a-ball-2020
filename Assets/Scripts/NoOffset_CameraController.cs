5d0602390ef37e45e3b6356a6fb81463db613229
Bozodo <aquinojt.1@gmail.com>
2020-02-20 11:30:40 -0500

    Added zoom in CameraController.cs to manually adjust the offset

--- Assets/Scripts/NoOffset_CameraController.cs
+++ Assets/Scripts/NoOffset_CameraController.cs
@@ -2,23 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class NoOffset_CameraController : MonoBehaviour
 {
     public GameObject player;
 
-    public Vector3 zoom;
-
-    private Vector3 offset;
-    
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position - zoom;
+        
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position;
     }
 }
